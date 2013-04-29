2355249e2ed2ebb89d19f3be7903d2a1236079a5
Steve Klabnik <steve@steveklabnik.com>
2013-04-29 16:59:52 -0600

    Simple CSV -> AR model import.
    Yup.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,8 +1,20 @@
+require 'csv'    
+
 class EventsController < ApplicationController
   def new
     @event = Event.new
   end
 
   def create
+    csv = params[:event][:csv]
+    csv = CSV.parse(csv.tempfile, :headers => true, :header_converters => :symbol)
+
+    @event = Event.create(:name => params[:event][:name])
+
+    csv.each do |row|
+      Attendee.create!(row.to_hash.merge({:event_id => @event.id}))
+    end
+
+    redirect_to @event, :notice => "Event created!"
   end
 end
