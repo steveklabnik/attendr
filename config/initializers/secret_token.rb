81b07ea8fea183034372358f50d48b7bbce0bab6
Steve Klabnik <steve@steveklabnik.com>
2013-04-29 16:05:22 -0600

    initial rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Attendr::Application.config.secret_key_base = '138e1a80171fa496f09f84fa77d175bcd15c36f7887c7493bd5735e7316232a6a39c03a9c3117e624ce2fe5bdb38c2129f2ca7907be2d26246520b5cd6f8a946'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Attendr::Application.config.session_store :cookie_store, key: '_attendr_session'
