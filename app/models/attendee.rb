cdbec5f0fdea058d4de3a998e16bf6cabedd312a
Steve Klabnik <steve@steveklabnik.com>
2013-04-30 14:01:45 -0600

    Add attendance count.

--- app/models/attendee.rb
+++ app/models/attendee.rb
@@ -1,2 +1,13 @@
 class Attendee < ActiveRecord::Base
+  def self.checked_in_total
+    checked_in = all.inject(0) do |sum, attendee|
+      if attendee.attended?
+        sum + 1
+      else
+        sum
+      end
+    end
+
+    "#{checked_in} / #{count}"
+  end
 end
