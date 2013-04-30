620c89b5a44333b39ad4d8934c99e89e7363d3dc
Steve Klabnik <steve@steveklabnik.com>
2013-04-30 11:01:40 -0600

    Implement AttendeeController#update.
    We only care about updating our attendance, nothing else should be mutable.
    
    We also need to redirect to the event_id because we have an 'event' attribute on
    attendees. Yay other people's naming conventions.

--- app/controllers/attendees_controller.rb
+++ app/controllers/attendees_controller.rb
@@ -1,2 +1,10 @@
 class AttendeesController < ApplicationController
+  def update
+    @attendee = Attendee.find(params[:id])
+    @attendee.attended = params[:attendee][:attended]
+    @attendee.save
+
+    # this has to be event_id because Attendees have an :event attribute.
+    redirect_to event_path(@attendee.event_id)
+  end
 end
