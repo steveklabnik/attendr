f16273d24b1a06f66370c4c91e3a73a5e0901462
Steve Klabnik <steve@steveklabnik.com>
2013-04-30 11:28:47 -0600

    Add 'didnt attend button'
    Now, we can set someone to attend and not attend, and we built a little
    helper to do it.

--- app/helpers/attendees_helper.rb
+++ app/helpers/attendees_helper.rb
@@ -1,2 +1,13 @@
 module AttendeesHelper
+  def attendance_fields_for(form, attendee)
+    if attendee.attended?
+      concat(content_tag(:td, "✔"))
+      concat(form.hidden_field :attended, :value => false)
+      concat(content_tag(:td, form.submit("Didn't attend")))
+    else
+      concat(form.hidden_field :attended, :value => true)
+      concat(content_tag(:td, form.submit("Attended")))
+      concat(content_tag(:td, "✔"))
+    end
+  end
 end
