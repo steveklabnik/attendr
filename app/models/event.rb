de1ff335f15e306b4c5b6b0bbef09a2d65f9423a
Steve Klabnik <steve@steveklabnik.com>
2013-04-30 16:34:16 -0600

    Bugfix: didn't return an event.
    When I originally wrote this code, it worked, then I extracted it into a model method,
    and then I committed.
    
    Turns out that actually broke things. :cry:
    
    Basically, the @event instance variable doens't help much once
    its' in the model. Fun.

--- app/models/event.rb
+++ app/models/event.rb
@@ -6,11 +6,13 @@ class Event < ActiveRecord::Base
   def self.from_csv(file, name)
     csv = CSV.open(file, headers: true, header_converters: :symbol, encoding: Encoding::UTF_8)
 
-    @event = Event.create(name: name)
+    event = Event.create(name: name)
 
     csv.each do |row|
-      Attendee.create!(row.to_hash.merge({event_id: @event.id}))
+      Attendee.create!(row.to_hash.merge({event_id: event.id}))
     end
+
+    event
   end
 
   def filename
